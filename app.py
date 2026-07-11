765b311d9d8fd2156c3ed60a760cfdbf6c70b0cd
Madhvrendra Pandey <madhvendrapandey2@gmail.com>
2026-07-11 20:21:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if not API_KEY:
 
 genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # -----------------------------
 # FUNCTIONS
